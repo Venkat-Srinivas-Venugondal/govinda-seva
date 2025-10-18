35cd1410a2f6eef4cb31b28e6da9aefe7e783aa0
venkat srinivas <venkatsrinivas207@gmail.com>
2025-10-18 12:30:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-336720068-e4a5b",
+  "appId": "1:320389393927:web:0086bd406f48d94278c4a2",
+  "apiKey": "AIzaSyBN-A_HO2wlAMJnpVwuqT5iAMb7fPpt-z0",
+  "authDomain": "studio-336720068-e4a5b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "320389393927"
+};
