8faace640a754b93431e45f0fabd3d540b269531
venkat srinivas <venkatsrinivas207@gmail.com>
2025-10-18 19:36:44 +0000

    https://w0.peakpx.com/wallpaper/416/319/HD-wallpaper-tirumala-balaji-god

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'w0.peakpx.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
