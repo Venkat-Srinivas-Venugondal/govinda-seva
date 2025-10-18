6a1d86e03a49b23a4166ae52784e64614870f42f
venkat srinivas <venkatsrinivas207@gmail.com>
2025-10-18 19:03:33 +0000

    use this link for landing page https://www.hlimg.com/images/places2see/7

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.hlimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
